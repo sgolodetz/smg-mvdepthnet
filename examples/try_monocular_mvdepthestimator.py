463cb5a1036ab003b32fef7afab4751fd7d6c13a
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-08 11:34:45 +0000

    Try depth estimation using monocular tracking.

---
+++ examples/try_monocular_mvdepthestimator.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+
+from argparse import ArgumentParser
+from typing import Dict, Optional
+
+from smg.imagesources import RGBFromRGBDImageSource, RGBImageSource
+from smg.mvdepthnet.mvdepthestimator import MVDepthEstimator
+from smg.openni import OpenNICamera, OpenNIRGBDImageSource
+from smg.pyorbslam2 import MonocularTracker
+from smg.rotory import DroneFactory, DroneRGBImageSource
+
+
+def main() -> None:
+    # Parse any command-line arguments.
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--output_dir", type=str,
+        help="an optional directory into which to save the keyframes"
+    )
+    parser.add_argument(
+        "--source_type", "-t", type=str, required=True, choices=("ardrone2", "kinect", "tello"),
+        help="the source type"
+    )
+    args: dict = vars(parser.parse_args())
+
+    image_source: Optional[RGBImageSource] = None
+    try:
+        # Construct the RGB image source.
+        # FIXME: This is duplicate code - factor it out.
+        source_type: str = args["source_type"]
+        if source_type == "kinect":
+            image_source = RGBFromRGBDImageSource(OpenNIRGBDImageSource(OpenNICamera(mirror_images=True)))
+        else:
+            kwargs: Dict[str, dict] = {
+                "ardrone2": dict(print_commands=False, print_control_messages=False, print_navdata_messages=False),
+                "tello": dict(print_commands=False, print_responses=False, print_state_messages=False)
+            }
+            image_source = DroneRGBImageSource(DroneFactory.make_drone(source_type, **kwargs[source_type]))
+
+        with MonocularTracker(
+            settings_file=f"settings-kinect.yaml", use_viewer=True,
+            voc_file="C:/orbslam2/Vocabulary/ORBvoc.txt", wait_till_ready=False
+        ) as tracker:
+            fx, fy, cx, cy = image_source.get_intrinsics()
+            model_path: str = "C:/Users/Stuart Golodetz/Downloads/MVDepthNet/opensource_model.pth.tar"
+
+            intrinsics: np.ndarray = np.array([
+                [fx, 0, cx],
+                [0, fy, cy],
+                [0, 0, 1]
+            ])
+
+            estimator: MVDepthEstimator = MVDepthEstimator(model_path, intrinsics)
+
+            reference_image: Optional[np.ndarray] = None
+            reference_pose: Optional[np.ndarray] = None
+
+            while True:
+                colour_image: np.ndarray = image_source.get_image()
+                cv2.imshow("Colour Image", colour_image)
+                c: int = cv2.waitKey(1)
+
+                if not tracker.is_ready():
+                    continue
+
+                pose: Optional[np.ndarray] = tracker.estimate_pose(colour_image)
+                if pose is None:
+                    continue
+
+                if c == ord('r'):
+                    reference_image = colour_image.copy()
+                    reference_pose = pose.copy()
+                    continue
+
+                if reference_image is not None:
+                    estimated_depth_image: np.ndarray = estimator.estimate_depth(
+                        colour_image, reference_image, np.linalg.inv(pose), np.linalg.inv(reference_pose)
+                    )
+                    cv2.imshow("Estimated Depth Image", estimated_depth_image / 2)
+                    cv2.waitKey(1)
+    finally:
+        # Terminate the image source once we've finished.
+        if image_source is not None:
+            image_source.terminate()
+
+
+if __name__ == "__main__":
+    main()
--- examples/try_rgbd_mvdepthestimator.py
+++ examples/try_rgbd_mvdepthestimator.py
@@ -4,13 +4,14 @@ import numpy as np
 from typing import Optional
 
 from smg.mvdepthnet.mvdepthestimator import MVDepthEstimator
+from smg.open3d import VisualisationUtil
 from smg.openni import OpenNICamera
-from smg.pyorbslam2 import MonocularTracker
+from smg.pyorbslam2 import RGBDTracker
 
 
 def main() -> None:
     with OpenNICamera(mirror_images=True) as camera:
-        with MonocularTracker(
+        with RGBDTracker(
                 settings_file=f"settings-kinect.yaml", use_viewer=True,
                 voc_file="C:/orbslam2/Vocabulary/ORBvoc.txt", wait_till_ready=False
         ) as tracker:
@@ -27,6 +28,7 @@ def main() -> None:
 
             reference_image: Optional[np.ndarray] = None
             reference_pose: Optional[np.ndarray] = None
+            estimated_depth_image: Optional[np.ndarray] = None
 
             while True:
                 colour_image, depth_image = camera.get_images()
@@ -37,7 +39,7 @@ def main() -> None:
                 if not tracker.is_ready():
                     continue
 
-                pose: Optional[np.ndarray] = tracker.estimate_pose(colour_image)  #, depth_image)
+                pose: Optional[np.ndarray] = tracker.estimate_pose(colour_image, depth_image)
                 if pose is None:
                     continue
 
@@ -46,38 +48,21 @@ def main() -> None:
                     reference_pose = pose.copy()
                     continue
 
+                if c == ord('v'):
+                    break
+
                 if reference_image is not None:
-                    estimated_depth_image: np.ndarray = estimator.estimate_depth(
+                    estimated_depth_image = estimator.estimate_depth(
                         colour_image, reference_image, np.linalg.inv(pose), np.linalg.inv(reference_pose)
                     )
                     cv2.imshow("Estimated Depth Image", estimated_depth_image / 2)
                     cv2.waitKey(1)
 
-        # estimator: MVDepthEstimator = MVDepthEstimator(model_path, intrinsics)
-        #
-        # left_image: np.ndarray = cv2.imread("D:/7scenes/heads/train/frame-000000.color.png")
-        # right_image: np.ndarray = cv2.imread("D:/7scenes/heads/train/frame-000055.color.png")
-        # left_pose: np.ndarray = np.array([
-        #     [9.9798417e-001, 2.5588147e-002, 5.5003788e-002, 9.5729552e-002],
-        #     [-3.4286145e-002, 9.8580331e-001, 1.6358595e-001, -4.1082110e-002],
-        #     [-5.0044306e-002, -1.6512756e-001, 9.8484284e-001, 1.3248709e-001],
-        #     [0.0000000e+000, 0.0000000e+000, 0.0000000e+000, 1.0000000e+000]
-        # ])
-        # right_pose: np.ndarray = np.array([
-        #     [9.9853259e-001, -4.9765650e-002, 9.9766739e-003, 2.9618734e-001],
-        #     [4.7499087e-002, 9.8555100e-001, 1.6177388e-001, -1.2100209e-001],
-        #     [-1.7887015e-002, -1.6104801e-001, 9.8662180e-001, -1.5921636e-002],
-        #     [0.0000000e+000, 0.0000000e+000, 0.0000000e+000, 1.0000000e+000]
-        # ])
-        #
-        # print(left_pose)
-        # print(right_pose)
-        #
-        # depth_image: np.ndarray = estimator.estimate_depth(left_image, right_image, left_pose, right_pose)
-        #
-        # plt.imshow(depth_image)
-        # plt.show()
-        # pass
+            if estimated_depth_image is not None:
+                estimated_depth_image = cv2.resize(estimated_depth_image, (640, 480), interpolation=cv2.INTER_NEAREST)
+                VisualisationUtil.visualise_rgbd_image(
+                    colour_image, estimated_depth_image, camera.get_colour_intrinsics()
+                )
 
 
 if __name__ == "__main__":
