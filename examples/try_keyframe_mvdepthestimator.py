d3cedcf5c7dd9a924584c4c91df1bc8377f375ce
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-13 14:45:53 +0000

    Try using two keyframes.

--- examples/try_keyframe_mvdepthestimator.py
+++ examples/try_keyframe_mvdepthestimator.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+from operator import itemgetter
 from typing import List, Optional, Tuple
 
 from smg.mvdepthnet.mvdepthestimator import MVDepthEstimator
@@ -51,45 +52,64 @@ def main() -> None:
                     baselines.append(CameraUtil.compute_baseline_p(tracker_w_t_c, keyframe_w_t_c))
                     look_angles.append(CameraUtil.compute_look_angle_p(tracker_w_t_c, keyframe_w_t_c))
 
-                # Try to choose a keyframe to use together with the current frame to estimate the depth.
-                best_keyframe_idx: int = -1
-                best_score: float = 0.0
+                # Score all of the keyframes with respect to the current frame.
+                scores: List[(int, float)] = []
                 smallest_baseline: float = 1000.0
                 smallest_look_angle: float = 1000.0
-                print(f"Keyframe Count: {len(keyframes)}")
+
                 for i in range(len(keyframes)):
                     smallest_baseline = min(baselines[i], smallest_baseline)
                     smallest_look_angle = min(look_angles[i], smallest_look_angle)
 
-                    if baselines[i] < 0.025:
-                        continue
-                    if look_angles[i] > 20.0:
-                        continue
+                    if baselines[i] < 0.025 or look_angles[i] > 20.0:
+                        scores.append((i, 0.0))
+                    else:
+                        # Adapted from the Mobile3DRecon paper.
+                        b_m: float = 0.15
+                        delta: float = 0.1
+                        alpha_m: float = 10.0
+                        w_b: float = np.exp(-(baselines[i] - b_m) ** 2 / delta ** 2)
+                        w_v: float = max(alpha_m / look_angles[i], 1)
+                        scores.append((i, w_b * w_v))
 
-                    # if not (0.05 <= baselines[i] <= 0.5):
-                    #     continue
-                    # if not (look_angles[i] <= 20.0):
-                    #     continue
+                # Try to choose up to two keyframes to use together with the current frame to estimate the depth.
+                if len(scores) > 0:
+                    # FIXME: There's no need to fully sort the list here.
+                    scores = sorted(scores, key=itemgetter(1), reverse=True)
+                    best_keyframe_idx, best_keyframe_score = scores[0]
+                    if best_keyframe_score > 0.0:
+                        _, best_keyframe_w_t_c = keyframes[best_keyframe_idx]
 
-                    # See the Mobile3DRecon paper.
-                    b_m: float = 0.15
-                    delta: float = 0.1
-                    alpha_m: float = 10.0
-                    w_b: float = np.exp(-(baselines[i] - b_m)**2 / delta**2)
-                    w_v: float = max(alpha_m / look_angles[i], 1)
-                    score: float = w_b * w_v
-                    if score > best_score:
-                        best_keyframe_idx = i
-                        best_score = score
+                        second_best_keyframe_idx: int = -1
+                        for i in range(1, len(scores)):
+                            keyframe_idx, keyframe_score = scores[i]
+                            if keyframe_score == 0.0:
+                                break
+                            _, keyframe_w_t_c = keyframes[keyframe_idx]
+                            inter_keyframe_baseline: float = CameraUtil.compute_baseline_p(
+                                best_keyframe_w_t_c, keyframe_w_t_c
+                            )
+                            if inter_keyframe_baseline > 0.025:
+                                second_best_keyframe_idx = i
+                                break
 
-                # If a suitable keyframe was found, estimate a depth image for the current frame, and show it.
-                if best_keyframe_idx >= 0:
-                    keyframe_image, keyframe_w_t_c = keyframes[best_keyframe_idx]
-                    estimated_depth_image: np.ndarray = depth_estimator.estimate_depth(
-                        colour_image, keyframe_image, tracker_w_t_c, keyframe_w_t_c
-                    )
-                    cv2.imshow("Estimated Depth Image", estimated_depth_image / 2)
-                    cv2.waitKey(1)
+                        aggregator: MVDepthEstimator.CostVolumeAggregator = MVDepthEstimator.CostVolumeAggregator()
+                        best_keyframe_image, _ = keyframes[best_keyframe_idx]
+                        aggregator.add_cost_volume(depth_estimator.make_cost_volume(
+                            colour_image, best_keyframe_image, tracker_w_t_c, best_keyframe_w_t_c
+                        ))
+
+                        if second_best_keyframe_idx > 0:
+                            second_best_keyframe_image, second_best_keyframe_w_t_c = keyframes[second_best_keyframe_idx]
+                            aggregator.add_cost_volume(depth_estimator.make_cost_volume(
+                                colour_image, second_best_keyframe_image, tracker_w_t_c, second_best_keyframe_w_t_c
+                            ))
+
+                        estimated_depth_image: np.ndarray = depth_estimator.estimate_depth_from_cost_volume(
+                            colour_image, aggregator.get_average_cost_volume()
+                        )
+                        cv2.imshow("Estimated Depth Image", estimated_depth_image / 2)
+                        cv2.waitKey(1)
 
                 # Check whether this frame should be a new keyframe. If so, add it to the list.
                 if smallest_baseline > 0.05 or smallest_look_angle > 5.0:
