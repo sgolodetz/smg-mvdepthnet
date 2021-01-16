53b6a5e6dadbb9e38921e3e380461f2afeee157d
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-16 20:42:48 +0000

    Continue tidying up MonocularDepthEstimator.

--- smg/mvdepthnet/monocular_depth_estimator.py
+++ smg/mvdepthnet/monocular_depth_estimator.py
@@ -15,36 +15,37 @@ class MonocularDepthEstimator:
     # CONSTRUCTOR
 
     def __init__(self, model_path: str, intrinsics: np.ndarray, *,
-                 debug: bool = False, max_baseline_before_keyframe: float = 0.05,
-                 max_consistent_depth_diff: float = 0.1, max_look_angle_before_keyframe: float = 5.0,
-                 max_triangulation_look_angle: float = 20.0, min_triangulation_baseline: float = 0.025):
+                 debug: bool = False, max_consistent_depth_diff: float = 0.1,
+                 max_rotation_before_keyframe: float = 5.0, max_rotation_for_triangulation: float = 20.0,
+                 max_translation_before_keyframe: float = 0.05, min_translation_for_triangulation: float = 0.025):
         """
         Construct a monocular depth estimator.
 
-        :param model_path:                      The path to the MVDepthNet model.
-        :param intrinsics:                      The 3x3 camera intrinsics matrix.
-        :param debug:                           Whether to show debug visualisations.
-        :param max_baseline_before_keyframe:    The maximum baseline (in m) there can be between the current position
-                                                and the position of the closest keyframe without triggering the
-                                                creation of a new keyframe.
-        :param max_consistent_depth_diff:       The maximum difference there can be between the depths estimated for
-                                                a pixel by the best and second best keyframes for those depths to be
-                                                considered sufficiently consistent.
-        :param max_look_angle_before_keyframe:  The maximum look angle (in degrees) there can be between the current
-                                                look vector and the look vector of the closest keyframe without
-                                                triggering the creation of a new keyframe.
-        :param max_triangulation_look_angle:    The maximum angle (in degrees) there can be between the look vector
-                                                of a keyframe and the current look vector for the keyframe to be used.
-        :param min_triangulation_baseline:      The maximum baseline (in m) there can be between the position of a
-                                                keyframe and the current position for the keyframe to be used.
+        :param model_path:                          The path to the MVDepthNet model.
+        :param intrinsics:                          The 3x3 camera intrinsics matrix.
+        :param debug:                               Whether to show debug visualisations.
+        :param max_translation_before_keyframe:     The maximum translation (in m) there can be between the current
+                                                    position and the position of the closest keyframe without
+                                                    triggering the creation of a new keyframe.
+        :param max_consistent_depth_diff:           The maximum difference there can be between the depths estimated
+                                                    for a pixel by the best and second best keyframes for those depths
+                                                    to be considered sufficiently consistent.
+        :param max_rotation_before_keyframe:        The maximum rotation (in degrees) there can be between the current
+                                                    look vector and the look vector of the closest keyframe without
+                                                    triggering the creation of a new keyframe.
+        :param max_rotation_for_triangulation:      The maximum rotation (in degrees) there can be between the look
+                                                    vector of a keyframe and the current look vector for the keyframe
+                                                    to be used.
+        :param min_translation_for_triangulation:   The minimum translation (in m) there can be between the position
+                                                    of a keyframe and the current position for the keyframe to be used.
         """
         self.__debug: bool = debug
         self.__keyframes: List[Tuple[np.ndarray, np.ndarray]] = []
-        self.__max_baseline_before_keyframe: float = max_baseline_before_keyframe
         self.__max_consistent_depth_diff: float = max_consistent_depth_diff
-        self.__max_look_angle_before_keyframe: float = max_look_angle_before_keyframe
-        self.__max_triangulation_look_angle: float = max_triangulation_look_angle
-        self.__min_triangulation_baseline: float = min_triangulation_baseline
+        self.__max_rotation_before_keyframe: float = max_rotation_before_keyframe
+        self.__max_rotation_for_triangulation: float = max_rotation_for_triangulation
+        self.__max_translation_before_keyframe: float = max_translation_before_keyframe
+        self.__min_translation_for_triangulation: float = min_translation_for_triangulation
         self.__multiview_depth_estimator: MultiviewDepthEstimator = MultiviewDepthEstimator(model_path, intrinsics)
 
     # PUBLIC METHODS
@@ -62,33 +63,33 @@ class MonocularDepthEstimator:
         """
         best_depth_image: Optional[np.ndarray] = None
 
-        # Compute the baselines (in m) and look angles (in degrees) with respect to any existing keyframes.
-        baselines: List[float] = []
-        look_angles: List[float] = []
+        # Compute the translations (in m) and (look) rotations (in degrees) with respect to any existing keyframes.
+        translations: List[float] = []
+        rotations: List[float] = []
         for _, keyframe_w_t_c in self.__keyframes:
-            baselines.append(CameraUtil.compute_baseline_p(tracker_w_t_c, keyframe_w_t_c))
-            look_angles.append(CameraUtil.compute_look_angle_p(tracker_w_t_c, keyframe_w_t_c))
+            translations.append(CameraUtil.compute_translation_p(tracker_w_t_c, keyframe_w_t_c))
+            rotations.append(CameraUtil.compute_look_rotation_p(tracker_w_t_c, keyframe_w_t_c))
 
         # Score all of the keyframes with respect to the current frame.
         scores: List[(int, float)] = []
-        smallest_baseline: float = np.inf
-        smallest_look_angle: float = np.inf
+        smallest_translation: float = np.inf
+        smallest_rotation: float = np.inf
 
         for i in range(len(self.__keyframes)):
-            smallest_baseline = min(baselines[i], smallest_baseline)
-            smallest_look_angle = min(look_angles[i], smallest_look_angle)
+            smallest_translation = min(translations[i], smallest_translation)
+            smallest_rotation = min(rotations[i], smallest_rotation)
 
-            if baselines[i] < self.__min_triangulation_baseline \
-                    or look_angles[i] > self.__max_triangulation_look_angle:
-                # If the baseline's too small, or the look angle's too large, force the score of this keyframe to 0.
+            if translations[i] < self.__min_translation_for_triangulation \
+                    or rotations[i] > self.__max_rotation_for_triangulation:
+                # If the translation's too small, or the rotation's too large, force the score of this keyframe to 0.
                 scores.append((i, 0.0))
             else:
                 # Otherwise, compute a score as per the Mobile3DRecon paper (but with different parameters).
                 b_m: float = 0.15
                 delta: float = 0.1
                 alpha_m: float = 10.0
-                w_b: float = np.exp(-(baselines[i] - b_m) ** 2 / delta ** 2)
-                w_v: float = max(alpha_m / look_angles[i], 1)
+                w_b: float = np.exp(-(translations[i] - b_m) ** 2 / delta ** 2)
+                w_v: float = max(alpha_m / rotations[i], 1)
                 scores.append((i, w_b * w_v))
 
         # Try to choose up to two keyframes to use together with the current frame to estimate the depth.
@@ -129,8 +130,8 @@ class MonocularDepthEstimator:
                     cv2.waitKey(1)
 
         # Check whether this frame should be a new keyframe. If so, add it to the list.
-        if smallest_baseline > self.__max_baseline_before_keyframe \
-                or smallest_look_angle > self.__max_look_angle_before_keyframe:
+        if smallest_translation > self.__max_translation_before_keyframe \
+                or smallest_rotation > self.__max_rotation_before_keyframe:
             self.__keyframes.append((colour_image.copy(), tracker_w_t_c.copy()))
 
         return best_depth_image
